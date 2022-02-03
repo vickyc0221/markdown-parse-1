d0003157fab526dae528be3bcddf0b7ad272fbd5
Chuan Peng <54980620+erichuanp@users.noreply.github.com>
2022-02-03 14:07:33 -0800

    make it as 2

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class MarkdownParseTest {
     @Test
     public void addition() {
-    assertEquals(3, 1 + 1);
+    assertEquals(2, 1 + 1);
     }
     
     @Test
