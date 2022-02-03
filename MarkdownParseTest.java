05d794834d4806e125e1732eef75f7f482f2db15
vickyc0221 <97704459+vickyc0221@users.noreply.github.com>
2022-02-03 14:23:35 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class MarkdownParseTest {
     @Test
     public void addition() {
-    assertEquals(2, 1 + 1);
+    assertEquals(3, 1 + 1);
     }
     
     @Test
