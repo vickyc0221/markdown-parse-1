2fe61dec8d2ff6767933455206e3b8f7924a3158
Chuan Peng <54980620+erichuanp@users.noreply.github.com>
2022-02-03 14:05:38 -0800

    make it as 3

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -39,7 +39,7 @@ public class MarkdownParse {
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of("breaking-test_2.md");
+		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
